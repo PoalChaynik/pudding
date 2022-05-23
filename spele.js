5061791c161e863c941f3762ac5a22944cb6f33f
PoalChaynik <alpolaop13@gmail.com>
2022-05-23 13:25:23 +0300

    23.05.2022

--- spele.js
+++ spele.js
@@ -7,4 +7,34 @@ let vecums = adrese[1]
 let region = adrese[2]
 console.log(adrese)
 
-document.querySelector('.virsraksts').innerHTML = 'Sveiks/a '+vards+'!'
+document.querySelector('.virsraksts').innerHTML = 'Sveiks '+vards+'!'
+
+let atvertieLaukumi = []
+let pedejieDivi = []
+
+function veiktGajienu(laukums)
+{
+    console.log('strādā')
+    let atvertsJaunsLaukums = false
+
+    if( atvertieLaukumi.indexOf(laukums) == -1 )
+    {
+    atvertsJaunsLaukums = true
+    pedejieDivi.push(laukums)
+    if(atvertsJaunsLaukums)
+    {
+        console.log('strada2')
+        document.querySelector('#'+laukums+' div').style.visibility="visible";
+            document.querySelector('#'+laukums+' div').style.opacity=1;
+
+    }
+    //atvertieLaukumi += laukums
+    }
+    else
+    {
+    atvertsJaunsLaukums = false
+    console.log(atvertsJaunsLaukums)
+    }
+    //console.log(atvertieLaukumi)
+    console.log(pedejieDivi)
+}
